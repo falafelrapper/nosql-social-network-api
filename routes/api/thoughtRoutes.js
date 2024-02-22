0bb5ae3dacb5e7bcad978b2600594c94d91d7ed1
Zachary Roy <vgd199@gmail.com>
2024-02-21 18:15:05 -0800

    changed a note in thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,11 +12,13 @@ const {
 // api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
+// /api/thoughts/:thoughtId/
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(createReaction);
 
+// /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
